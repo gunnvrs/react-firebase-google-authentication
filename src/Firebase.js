0a742445893d008e8ef93c58a9c0cba67245e8a1
gunnvrs <gunn2545@gmail.com>
2023-02-05 17:12:58 +0700

    first gunn commit

--- src/Firebase.js
+++ src/Firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyANfoLDGZsVAl8NwHJUlgLrUynJJMaSsg0",
-  authDomain: "auth-8b1ef.firebaseapp.com",
-  projectId: "auth-8b1ef",
-  storageBucket: "auth-8b1ef.appspot.com",
-  messagingSenderId: "445273762769",
-  appId: "1:445273762769:web:81f403cae7cb5fe3760ef0",
+  apiKey: "AIzaSyAHBBx5buvkempJoiAXQZBGwVAAOYZH3cw",
+  authDomain: "itzwebapp.firebaseapp.com",
+  projectId: "itzwebapp",
+  storageBucket: "itzwebapp.appspot.com",
+  messagingSenderId: "368927960084",
+  appId: "1:368927960084:web:79e207d2bbe32ad0784b01"
 };
 
 const app = initializeApp(firebaseConfig);

70cb0d7e9aed49c90145648ce74a183ba14e697b
gunnvrs <gunn2545@gmail.com>
2023-02-06 20:40:51 +0700

    make css better

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <button class="login-with-google-btn" onClick={signInWithGoogle}>
         Sign in with Google
       </button>
-      <h1>{localStorage.getItem("name")}</h1>
-      <h1>{localStorage.getItem("email")}</h1>
+      <googleinname>{localStorage.getItem("name")}</googleinname>
+      <googleinemail>{localStorage.getItem("email")}</googleinemail>
       <img src={localStorage.getItem("profilePic")} />
     </div>
   );
